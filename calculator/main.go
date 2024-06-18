4315ad6c403ba3b98c8a83ade083851009dc572f
Kuba <urbanski.jakub77@gmail.com>
2024-06-18 13:46:45 +0200

    feat: basic calculator

---
+++ calculator/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var val1, val2 int
+	// var result float32
+	var operation string
+
+	fmt.Print("Type in the first number: ")
+	fmt.Scan(&val1)
+
+	fmt.Print("Type in the second number: ")
+	fmt.Scan(&val2)
+
+	fmt.Print("Type in the operation name or its number (1.add, 2.subtract, 3.multiply, 4.divide): ")
+	fmt.Scan(&operation)
+
+	operation = strings.ToLower(operation)
+
+	fmt.Printf("The result of %s operation of given numbers (%d, %d) is ", operation, val1, val2)
+
+	switch operation {
+	case "1", "1.", "add":
+		fmt.Print(val1 + val2)
+	case "2", "2.", "subtract":
+		fmt.Print(val1 - val2)
+	case "3", "3.", "multiply":
+		fmt.Print(val1 * val2)
+	case "4", "4.", "divide":
+		fmt.Print(float32(val1) / float32(val2))
+	default:
+		fmt.Print("WRONG OPERATION NAME OR TYPE OF NUMBERS!")
+	}
+
+	fmt.Printf("\n")
+}
